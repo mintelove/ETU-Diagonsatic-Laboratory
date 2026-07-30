610a27b70751348ab8fafc6c025e71a07ab049e2
mintelove <mammy2012m@gmail.com>
2026-07-30 18:02:55 +0300

    fix(frontend): import useParams from react-router-dom in PublicReportPage to resolve runtime error

--- frontend/src/pages/PublicReportPage.jsx
+++ frontend/src/pages/PublicReportPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react';
+import { useParams } from 'react-router-dom';
 import { FlagBadge } from '../utils/flagHelper.jsx';
 import '../styles/pages/investigation.css';
 
